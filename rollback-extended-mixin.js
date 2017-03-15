6b03c2467d13e5939b57e0765d6402f9ba633e7b
root <xang.xiong.email@gmail.com>
2017-03-15 05:51:07 +0000

    adding minification

--- rollback-extended-mixin.js
+++ rollback-extended-mixin.js
@@ -20,6 +20,8 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
+ *
+ * @version 1.1.0
  */
 (function(window, factory) {
 	if(typeof define === 'function' && define.amd) {
